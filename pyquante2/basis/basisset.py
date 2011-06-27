3850084430aa35b4c2cf59ca74ae9713eac1ae99
Rick Muller <rmuller@sandia.gov>
2011-06-27 09:53:32 -0600

    Moved the basisset class into its own file. Renamed the defaults file to settings.

---
+++ pyquante2/basis/basisset.py
@@ -0,0 +1,42 @@
+"""\
+Basis set constructor
+
+>>> from pyquante2.geo.samples import h
+>>> bfs = basisset(h,'sto3g')
+
+Haven't figured out how to test the basis set yet. Print it out?
+
+Copyright (c) 2004, Richard P. Muller. All Rights Reserved. 
+
+ PyQuante version 1.2 and later is covered by the modified BSD
+ license. Please see the file LICENSE that is part of this
+ distribution. 
+"""
+
+from pyquante2 import settings
+from pyquante2.basis.cgbf import cgbf
+from pyquante2.basis.tools import get_basis_data,sym2pow
+
+class basisset:
+    def __init__(self,atoms,name,**kwargs):
+        self.bfs = []
+        basis_data = get_basis_data(name)
+        omit_f = kwargs.get('omit_f',settings.omit_f)
+        for atom in atoms:
+            for sym,prims in basis_data[atom.atno]:
+                if omit_f and sym == 'F': continue
+                exps = [e for e,c in prims]
+                coefs = [c for e,c in prims]
+                for power in sym2pow[sym]:
+                    self.bfs.append(cgbf(atom.r,power,exps,coefs))
+        return
+
+    def __getitem__(self,i): return self.bfs.__getitem__(i)
+    def __repr__(self): return repr(self.bfs)
+
+# Can define shellbasisset here, which is essentially a normal basis set, but has the functions
+# shorted into shells. Overload the normal basis set.
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
