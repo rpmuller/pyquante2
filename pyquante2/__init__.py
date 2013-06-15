c16b744e52fc8a5e5da802be7c19bac640f818d9
Rick Muller <rpmuller@gmail.com>
2013-06-15 12:25:25 -0600

    More intelligent list of module-level imports

--- pyquante2/__init__.py
+++ pyquante2/__init__.py
@@ -1,5 +1,9 @@
-from pyquante2.basis.pgbf import pgbf
+from pyquante2.basis.basisset import basisset
 from pyquante2.basis.cgbf import cgbf,sto
+from pyquante2.basis.pgbf import pgbf
+from pyquante2.geo.molecule import molecule
+from pyquante2.geo.samples import *
 from pyquante2.ints.one import S,T,V
+from pyquante2.scf.hamiltonians import rhf
 
 
