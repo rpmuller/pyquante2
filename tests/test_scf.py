d08d704a10ee419ba6126e26a375feff1db3d281
Rick Muller <rpmuller@gmail.com>
2013-06-15 18:28:13 -0600

    Simplified tests and added uhf tests

--- tests/test_scf.py
+++ tests/test_scf.py
@@ -1,27 +1,25 @@
 import unittest
-from pyquante2 import molecule,rhf,h2,h2o,lih,basisset
-from pyquante2.scf.iterators import simple,averaging
+from pyquante2 import molecule,rhf,uhf,h2,h2o,lih,basisset
 
 class test_scf(unittest.TestCase):
     def test_h2(self):
         bfs = basisset(h2,'sto3g')
-        h2_rhf = rhf(h2,bfs)
-        ens = h2_rhf.converge(simple)
-        self.assertAlmostEqual(h2_rhf.energy(),-1.117099582955609)
+        solver = rhf(h2,bfs)
+        ens = solver.converge()
+        self.assertAlmostEqual(solver.energy,-1.117099582955609)
 
     def test_lih(self):
         bfs = basisset(lih,'sto3g')
-        lih_rhf = rhf(lih,bfs)
-        #E,c = lih_rhf.iterate()
-        ens = lih_rhf.converge(simple)
-        self.assertAlmostEqual(lih_rhf.energy(),-7.8607437) # Jaguar energy for 6-31G**
+        solver = rhf(lih,bfs)
+        ens = solver.converge()
+        self.assertAlmostEqual(solver.energy,-7.8607437)
 
     def test_lih_averaging(self):
+        from pyquante2.scf.iterators import averaging
         bfs = basisset(lih,'sto3g')
-        lih_rhf = rhf(lih,bfs)
-        #E,c = lih_rhf.iterate()
-        ens = lih_rhf.converge(averaging)
-        self.assertAlmostEqual(lih_rhf.energy(),-7.8607437244557961)
+        solver = rhf(lih,bfs)
+        ens = solver.converge(averaging)
+        self.assertAlmostEqual(solver.energy,-7.8607375733271088)
 
     def test_h4(self):
         h4 = molecule([
@@ -32,19 +30,34 @@ class test_scf(unittest.TestCase):
             ],
                       units='Angstrom')
         bfs = basisset(h4,'sto3g')
-        h4_rhf = rhf(h4,bfs)
-        ens = h4_rhf.converge(simple)
-        self.assertAlmostEqual(h4_rhf.energy(),-2.234185653441159)
+        solver = rhf(h4,bfs)
+        ens = solver.converge()
+        self.assertAlmostEqual(solver.energy,-2.234185653441159)
         # This is not quite equal to 2x the h2 energy, but very close
 
     def test_h2o(self):
         bfs = basisset(h2o,'sto3g')
-        h2o_rhf = rhf(h2o,bfs)
-        ens = h2o_rhf.converge(simple)
-        self.assertAlmostEqual(h2o_rhf.energy(),-74.959856073494194)
+        solver = rhf(h2o,bfs)
+        ens = solver.converge()
+        self.assertAlmostEqual(solver.energy,-74.959856073494194)
 
     def test_h2o_averaging(self):
+        from pyquante2.scf.iterators import averaging
         bfs = basisset(h2o,'sto3g')
-        h2o_rhf = rhf(h2o,bfs)
-        ens = h2o_rhf.converge(averaging)
-        self.assertAlmostEqual(h2o_rhf.energy(),-74.959856073494194)
+        solver = rhf(h2o,bfs)
+        ens = solver.converge(averaging)
+        self.assertAlmostEqual(solver.energy,-74.959846854926553)
+
+    def test_oh(self):
+        from pyquante2 import oh
+        bfs = basisset(oh,'sto3g')
+        solver = uhf(oh,bfs)
+        Es = solver.converge()
+        self.assertAlmostEqual(solver.energy,-130.48582965653802)
+
+    def test_li(self):
+        from pyquante2 import li
+        bfs = basisset(li,'sto3g')
+        solver = uhf(li,bfs)
+        Es = solver.converge()
+        self.assertAlmostEqual(solver.energy,-12.06529283901496)
