731e1cb5928aba2fbe0af0e278610a52e7238b69
Rick Muller <rpmuller@gmail.com>
2013-06-15 18:20:25 -0600

    Removed forces/velocities until needed

--- pyquante2/geo/atom.py
+++ pyquante2/geo/atom.py
@@ -17,26 +17,18 @@ array([ 0.,  0.,  0.])
 from numpy import array
 
 class atom:
-    def __init__(self,atno,x,y,z,atid=0,fx=0.0,fy=0.0,fz=0.0,vx=0.0,vy=0.0,vz=0.0,**kwargs):
+    def __init__(self,atno,x,y,z,**kwargs):
         self.atno = atno
         self.r = array([x,y,z],'d')
-        self.atid = atid
-        self.f = array([fx,fy,fz],'d')
-        self.vel = array([vx,vy,vz],'d')
-
         self.units = kwargs.get('units','bohr').lower()
         assert self.units[:4] in ['bohr','angs']
         if not self.units == 'bohr':
             self.r /= 0.52918
-
         return
 
     def atuple(self): return (self.atno,self.r[0],self.r[1],self.r[2])
-
     def __repr__(self): return repr(self.atuple())
-
-    def __getitem__(self, i):
-        return self.r[i]
+    def __getitem__(self, i): return self.r[i]
 
 if __name__ == '__main__':
     import doctest
