48a102dfc3af0069e593847f0725b3fc80daa18c
Rick Muller <rpmuller@gmail.com>
2013-06-20 14:24:46 -0600

    updated plotting driver. still doesn't work.

--- tests/plots.py
+++ tests/plots.py
@@ -1,4 +1,12 @@
+from pyquante2 import basisset,rhf,h2
+from pyquante2.graphics.vtk import vtk_orbital
 
 bfs = basisset(h2,'sto3g')
 solver = rhf(h2,bfs)
 ens = solver.converge()
+
+# Note: these orbitals are not coming out symmetric. Why not??
+print solver
+print solver.orbs
+vtk_orbital(h2,solver.orbs,bfs)
+
