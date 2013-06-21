4c456bb9e42bfc7df3962807a60d6d297a89805d
Rick Muller <rpmuller@gmail.com>
2013-06-21 08:27:06 -0600

    Plot orb doesn't work; plot bf does
    Can't understand this: plot bf works perfectly, but the only slightly
    more complicated plot orb function doesn't work (always gives a
    symmetric combination of the bfs regardless of the orb values).

--- pyquante2/graphics/lineplot.py
+++ pyquante2/graphics/lineplot.py
@@ -1,36 +1,33 @@
 import numpy as np
 import pylab as pl
 
-def lineplot(points,orbs,bfs,doshow=False):
+def lineplot_orb(points,orbs,bfs,doshow=False):
+    zvals = [z for (x,y,z) in points]
     for orb in orbs:
-        f = np.zeros(len(points),'d')
-        for c in orb:
-            for bf in bfs:
-                for i,(x,y,z) in enumerate(points):
-                    f[i] += c*bf(x,y,z)
-        print orb
-        pl.plot(points,f,label=str(tuple(orb))) # appears to be called 3x per orb???
-    pl.legend()
-    pl.savefig('pyq_orb.png')
-    
+        pl.plot(zvals,orb_on_points(points,orb,bfs))
+    #pl.savefig('pyq_orb.png')
     if doshow:
         pl.show()
     return
 
+def bf_on_points(points,bf): return np.array([bf(*point) for point in points],'d')
+def orb_on_points(points,orb,bfs):
+    f = np.zeros(len(points),'d')
+    for c in orb:
+        for bf in bfs:
+            f = f + c*bf_on_points(points,bf)
+    return f
+
 def lineplot_bfs(points,bfs,doshow=False):
+    zvals = [z for (x,y,z) in points]
     for bf in bfs:
-        f = np.zeros(len(points),'d')
-        for i,(x,y,z) in enumerate(points):
-            f[i] += bf(x,y,z)
-        pl.plot(points,f)
-    #pl.savefig('pyq_bf.png')
-    
+        pl.plot(zvals,bf_on_points(points,bf))
     if doshow:
         pl.show()
     return
         
 
-def plot_fake():
+def test_plot_bfs():
     from pyquante2 import basisset,h2
     bfs = basisset(h2,'sto3g')
     zvals = np.linspace(-5,5)
@@ -38,7 +35,7 @@ def plot_fake():
     lineplot_bfs(points,bfs,True)
     return
 
-def plot_fake2():
+def test_plot_orbs():
     from pyquante2 import basisset,h2
     bfs = basisset(h2,'sto3g')
     orbs = np.array([[1.0,0.0],
@@ -46,10 +43,10 @@ def plot_fake2():
     
     zvals = np.linspace(-5,5)
     points = [(0,0,z) for z in zvals]
-    lineplot(points,orbs,bfs,True)
+    lineplot_orb(points,orbs,bfs,True)
     return
 
 if __name__ == '__main__':
-    plot_fake()
+    test_plot_orbs()
 
         
