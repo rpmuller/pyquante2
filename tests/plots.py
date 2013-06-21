db0e9bf506322437889a0d0f96b8626cf54d6266
Rick Muller <rpmuller@gmail.com>
2013-06-21 08:06:59 -0600

    More debugging. Still doesn't work.

--- tests/plots.py
+++ tests/plots.py
@@ -1,12 +1,25 @@
+import numpy as np
 from pyquante2 import basisset,rhf,h2
 from pyquante2.graphics.vtk import vtk_orbital
 
-bfs = basisset(h2,'sto3g')
-solver = rhf(h2,bfs)
-ens = solver.converge()
+def plot_h2():
+    bfs = basisset(h2,'sto3g')
+    solver = rhf(h2,bfs)
+    ens = solver.converge()
 
-# Note: these orbitals are not coming out symmetric. Why not??
-print solver
-print solver.orbs
-vtk_orbital(h2,solver.orbs,bfs)
+    # Note: these orbitals are not coming out symmetric. Why not??
+    print solver
+    print solver.orbs
+    vtk_orbital(h2,solver.orbs,bfs)
+
+def plot_fake():
+    bfs = basisset(h2,'sto3g')
+    orbs = np.array([[1.0,1.0],
+                     [1.0,-1.0]],'d')
+    
+    vtk_orbital(h2,orbs,bfs)
+    return
+
+if __name__ == '__main__':
+    plot_fake()
 
