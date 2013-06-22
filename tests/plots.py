250e2af08bbf5d7a76125f05e8c8088bf3a94633
Rick Muller <rpmuller@gmail.com>
2013-06-22 16:58:50 -0600

    Moved h2 plotting here and cleaned routine.

--- tests/plots.py
+++ tests/plots.py
@@ -1,14 +1,22 @@
 import numpy as np
 from pyquante2 import basisset,rhf,h2
-from pyquante2.graphics.vtk import vtk_orbital
-from pyquante.graphics.lineplot import test_plot_orbs,test_plot_bfs
-from pyquante.graphics.contourplot import test_contour
+from pyquante2.graphics.vtkplot import vtk_orbs
+from pyquante2.graphics.lineplot import test_plot_orbs,test_plot_bfs,lineplot_orbs
+from pyquante2.graphics.contourplot import test_contour
 
-def lineplot_orbs(): return test_plot_orbs()
-def lineplot_bfs(): return test_plot_bfs()
+def test_lineplot_orbs(): return test_plot_orbs()
+def test_lineplot_bfs(): return test_plot_bfs()
 def contour_orb(): return test_contour(True)
     
-def plot_h2():
+def plot_h2_lineplot():
+    bfs = basisset(h2,'6-31G')
+    solver = rhf(h2,bfs)
+    solver.converge()
+    points = [(0,0,z) for z in np.linspace(-5,5)]
+    lineplot_orbs(points,solver.orbs[:,:2],bfs,True)
+    return
+
+def plot_h2_vtk():
     bfs = basisset(h2,'sto3g')
     solver = rhf(h2,bfs)
     ens = solver.converge()
@@ -27,5 +35,5 @@ def plot_orbs():
     return
 
 if __name__ == '__main__':
-    plot_h2()
+    plot_h2_lineplot()
 
