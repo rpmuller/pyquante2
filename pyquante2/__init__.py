b7f0dbe9775a8f6b5a97d440bf04045e3c0ddab4
Rick Muller <rpmuller@gmail.com>
2013-06-22 16:56:35 -0600

    Added imports of plotting routines

--- pyquante2/__init__.py
+++ pyquante2/__init__.py
@@ -3,6 +3,9 @@ from pyquante2.basis.cgbf import cgbf,sto
 from pyquante2.basis.pgbf import pgbf
 from pyquante2.geo.molecule import molecule
 from pyquante2.geo.samples import *
+from pyquante2.graphics.lineplot import lineplot_orbs
+from pyquante2.graphics.contourplot import contour_xy,contour_yz
+from pyquante2.graphics.vtkplot import vtk_orbs
 from pyquante2.ints.one import S,T,V
 from pyquante2.scf.hamiltonians import rhf,uhf
 
