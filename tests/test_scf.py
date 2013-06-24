d749bcbe589779d5edb2cf2e870343d17fe22fb0
Rick Muller <rpmuller@gmail.com>
2013-06-24 07:44:12 -0600

    Started profiling test suite
    There appears to be a difference between running the test suite from
    nosetests and running it interactively. The answers are slightly
    difference (hence the additional limits to only match unit tests to 6
    decimal places) and the tests run *much* faster when run interactively.
    It's almost like I'm importing the python version from nose, and the
    cython version when I run from the command line.

--- tests/test_scf.py
+++ tests/test_scf.py
@@ -45,21 +45,21 @@ class test_scf(unittest.TestCase):
         bfs = basisset(h2o,'sto3g')
         solver = rhf(h2o,bfs)
         ens = solver.converge()
-        self.assertAlmostEqual(solver.energy,-74.959856073494194)
+        self.assertAlmostEqual(solver.energy,-74.959856073494194,6)
 
     def test_h2o_averaging(self):
         from pyquante2.scf.iterators import averaging
         bfs = basisset(h2o,'sto3g')
         solver = rhf(h2o,bfs)
         ens = solver.converge(averaging)
-        self.assertAlmostEqual(solver.energy,-74.959846854926553)
+        self.assertAlmostEqual(solver.energy,-74.959846854926553,6)
 
     def test_oh(self):
         from pyquante2 import oh
         bfs = basisset(oh,'sto3g')
         solver = uhf(oh,bfs)
         Es = solver.converge()
-        self.assertAlmostEqual(solver.energy,-74.14666861386641)
+        self.assertAlmostEqual(solver.energy,-74.14666861386641,6)
 
     def test_li(self):
         from pyquante2 import li
@@ -67,3 +67,33 @@ class test_scf(unittest.TestCase):
         solver = uhf(li,bfs)
         Es = solver.converge()
         self.assertAlmostEqual(solver.energy,-7.2301642412807379)
+
+
+def runsuite(verbose=True):
+    # To use psyco, uncomment this line:
+    #import psyco; psyco.full()
+    if verbose: verbosity=2
+    else: verbosity=1
+    # If you want more output, uncomment this line:
+    #logging.basicConfig(format="%(message)s",level=logging.DEBUG)
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_scf)
+    unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    # Running without verbosity is equivalent to replacing the above
+    # two lines with the following:
+    #unittest.main()
+    return
+
+def debugsuite():
+    import cProfile,pstats
+    cProfile.run('runsuite()','prof')
+    prof = pstats.Stats('prof')
+    prof.strip_dirs().sort_stats('time').print_stats(15)
+
+if __name__ == '__main__':
+    import sys
+    if "-d" in sys.argv:
+        debugsuite()
+    else:
+        runsuite()
+    
+    
