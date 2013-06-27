50a7771b28f36ff9c468f389fb9a8ccbe6da29b5
Rick Muller <rpmuller@gmail.com>
2013-06-27 14:10:47 -0600

    Added USCF and Averaging iterators

--- pyquante2/scf/iterators.py
+++ pyquante2/scf/iterators.py
@@ -31,6 +31,45 @@ class SCFIterator:
         self.Eold = E
         return E
 
+class USCFIterator(SCFIterator):
+    def __init__(self,H,c=None,tol=1e-5,maxiters=100):
+        SCFIterator.__init__(self,H,c,tol,maxiters)
+        self.nup,self.ndown = self.H.geo.nup(),self.H.geo.ndown()
+        self.cup = self.cdown = self.c
+        return
+
+    def next(self):
+        self.iterations += 1
+        if self.iterations > self.maxiters:
+            raise StopIteration
+        Dup = dmat(self.cup,self.nup)
+        Ddown = dmat(self.cdown,self.ndown)
+        self.cup,self.cdown = self.H.update(Dup,Ddown)
+        E = self.H.energy
+        if abs(E-self.Eold) < self.tol:
+            self.converged = True
+            raise StopIteration
+        self.Eold = E
+        return E
+
+class AveragingIterator(SCFIterator):
+    def __init__(self,H,c=None,fraction=0.5,tol=1e-5,maxiters=100):
+        SCFIterator.__init__(self,H,c,tol,maxiters)
+        self.fraction = fraction
+        self.Dold = dmat(self.c,self.H.geo.nocc())
+        return
+
+    def next(self):
+        D = (1-self.fraction)*self.Dold + self.fraction*dmat(self.c,self.H.geo.nocc())
+        self.Dold = D
+        self.c = self.H.update(D)
+        E = self.H.energy
+        if abs(E-self.Eold) < self.tol:
+            self.converged = True
+            raise StopIteration
+        self.Eold = E
+        return E
+        
 def simple(H,c=None,tol=1e-5,maxiters=100):
     """
     Simple SCF iterator.
