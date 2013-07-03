d7e0fffc6ff5514be592e4c003eea0410f534f9f
Rick Muller <rpmuller@gmail.com>
2013-07-02 19:31:11 -0600

    Test cases for MP2

---
+++ tests/test_mp2.py
@@ -0,0 +1,19 @@
+import unittest
+from pyquante2 import rhf,basisset,h2,mp2
+
+class test_mp2(unittest.TestCase):
+    def test_h2(self):
+        bfs = basisset(h2,'6-31g**')
+        solver=rhf(h2,bfs)
+        solver.converge()
+        nvirt = len(bfs)-h2.nocc()
+        emp2 = mp2(solver.i2._2e_ints,solver.orbs,solver.orbe,h2.nocc(),len(bfs)-h2.nocc())
+        self.assertAlmostEqual(emp2,-0.02632654197486595)
+        return
+
+if __name__ == '__main__':
+    import sys
+    if "-d" in sys.argv:
+        debugsuite()
+    else:
+        runsuite()
