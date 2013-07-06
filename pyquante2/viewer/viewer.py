72273bd08380401498e41bac71ed9b07da2c5efb
Rick Muller <rpmuller@gmail.com>
2013-07-06 09:30:28 -0600

    Updated viewer

--- pyquante2/viewer/viewer.py
+++ pyquante2/viewer/viewer.py
@@ -3,9 +3,9 @@
 from pyglet.gl import *
 from pyglet import window
 from trackball_camera import TrackballCamera
-from Element import Element,sym2no
+from pyquante2.geo.elements import sym2no
 import math
-import numpy
+import numpy as np
 
 # Defaults for kwarg-passed options
 defaults = {
@@ -24,45 +24,6 @@ defaults = {
     'lightpos'    : (10,4,10,1), # Default position for lighting
     'lightcolor'  : (1,1,1,1),   # Default color for lighting
     }
-
-def parseline(line,format):
-    """\
-    Given a line (a string actually) and a short string telling
-    how to format it, return a list of python objects that result.
-
-    The format string maps words (as split by line.split()) into
-    python code:
-    x   ->    Nothing; skip this word
-    s   ->    Return this word as a string
-    i   ->    Return this word as an int
-    d   ->    Return this word as an int
-    f   ->    Return this word as a float
-
-    Basic parsing of strings:
-    >>> parseline('Hello, World','ss')
-    ['Hello,', 'World']
-
-    You can use 'x' to skip a record; you also don't have to parse
-    every record:
-    >>> parseline('1 2 3 4','xdd')
-    [2, 3]
-
-    >>> parseline('C1   0.0  0.0 0.0','sfff')
-    ['C1', 0.0, 0.0, 0.0]
-
-    Should this return an empty list?
-    >>> parseline('This line wont be parsed','xx')
-    """
-    xlat = {'x':None,'s':str,'f':float,'d':int,'i':int}
-    result = []
-    words = line.split()
-    for i in range(len(format)):
-        f = format[i]
-        trans = xlat.get(f,None)
-        if trans: result.append(trans(words[i]))
-    if len(result) == 0: return None
-    if len(result) == 1: return result[0]
-    return result
 
 def draw_sphere(x,y,z,red,green,blue,rad,**kwargs):
     mat_draw = kwargs.get('mat_draw',defaults['mat_draw'])
@@ -163,9 +124,16 @@ class TBWindow:
     def __init__(self,width=defaults['width'],height=defaults['height']):
         self.width = width
         self.height = height
-        self.config = Config(double_buffer=True, depth_size=24)
-        self.win = window.Window(visible=True,resizable=True,
-                                 config=self.config, caption='Sam')
+        #self.config = Config(double_buffer=True, depth_size=24)
+        #self.win = window.Window(visible=True,resizable=True,
+        #                         config=self.config, caption='Viewer')
+        platform = pyglet.window.get_platform()
+        display = platform.get_default_display()
+        screen = display.get_default_screen()
+        template = Config(alpha_size=8,double_buffer=True,depth_size=24)
+        config = screen.get_best_config(template)
+        context = config.create_context(None)
+        self.win = window.Window(context=context)
 
         # set callbacks
         self.win.on_resize = self.on_resize
@@ -250,59 +218,31 @@ class TBWindow:
                 norm1(y,self.height))
         return
 
-class Position:
-    def __init__(self,x=0,y=0,z=0):
-        self._pos = numpy.array((x,y,z),'d')
-        return
-
-    def get(self): return self._pos
-
-    def translate(self,d):
-        if type(d) == type((0,)):
-            self._pos += numpy.array(d,'d')
-        self._pos += d
-        return
-
-    def dist(self,other):
-        d = self._pos - other.get()
-        return math.sqrt(numpy.dot(d,d))
-
-    def __repr__(self): return "(%.4f,%.4f,%.4f)" % tuple(self._pos)
-
 class Sphere:
     def __init__(self,x,y,z,r,g,b,rad):
-        self._pos = Position(x,y,z)
-        self._color = Color(r,g,b)
+        self._pos = (x,y,z)
+        self._color = (r,g,b)
         self._rad = rad
 
     def draw(self):
-        x,y,z = self._pos.get()
-        r,g,b = self._color.get()
+        x,y,z = self._pos
+        r,g,b = self._color
         draw_sphere(x,y,z,r,g,b,self._rad)
 
     def __repr__(self):
         return "Sphere(%s,%s,%f)" % (self._pos,self._color,self._rad)
 
-class Color:
-    def __init__(self,r,g,b):
-        self._color = (r,g,b)
-        return
-
-    def get(self): return self._color
-
-    def __repr__(self): return "(%.2f,%.2f,%.2f)" % self._color
-
 class Cylinder:
     def __init__(self,x1,y1,z1,x2,y2,z2,r,g,b,rad):
-        self._start = Position(x1,y1,z1)
-        self._end = Position(x2,y2,z2)
-        self._color = Color(r,g,b)
+        self._start = (x1,y1,z1)
+        self._end = (x2,y2,z2)
+        self._color = (r,g,b)
         self._rad = rad
 
     def draw(self):
-        x1,y1,z1 = self._start.get()
-        x2,y2,z2 = self._end.get()
-        r,g,b = self._color.get()
+        x1,y1,z1 = self._start
+        x2,y2,z2 = self._end
+        r,g,b = self._color
         draw_cylinder(x1,y1,z1,x2,y2,z2,r,g,b,self._rad)
 
     def __repr__(self):
@@ -311,146 +251,61 @@ class Cylinder:
 
 class Line:
     def __init__(self,x1,y1,z1,x2,y2,z2,r=255,g=255,b=255):
-        self._start = Position(x1,y1,z1)
-        self._end = Position(x2,y2,y2)
-        self._color = Color(r,g,b)
+        self._start = (x1,y1,z1)
+        self._end = (x2,y2,y2)
+        self._color = (r,g,b)
 
     def draw(self):
-        x1,y1,z1 = self._start.get()
-        x2,y2,z2 = self._end.get()
-        r,g,b = self._color.get()
+        x1,y1,z1 = self._start
+        x2,y2,z2 = self._end
+        r,g,b = self._color
         draw_line(x1,y1,z1,x2,y2,z2,r,g,b)
 
-class UC:
-    def __init__(self,A,B,C,origin=(0,0,0)):
-        self._origin = Position(*origin)
-        self._A = Position(*A)
-        self._B = Position(*B)
-        self._C = Position(*C)
-        return
-
-    def shapes(self):
-        xo,yo,zo = self._origin.get()
-        xa,ya,za = self._A.get()
-        xb,yb,zb = self._B.get()
-        xc,yc,zc = self._C.get()
-        s = [Line(xo,yo,zo,xo+xa,yo+ya,zo+za),
-             Line(xo,yo,zo,xo+xb,yo+yb,zo+zb),
-             Line(xo,yo,zo,xo+xc,yo+yc,zo+zc),
-             Line(xo+xa+xb+xc,zo+ya+yb+yc,zo+za+zb+zc,
-                  xo+xa,yo+ya,zo+za),
-             Line(xo+xa+xb+xc,zo+ya+yb+yc,zo+za+zb+zc,
-                  xo+xb,yo+yb,zo+zb),
-             Line(xo+xa+xb+xc,zo+ya+yb+yc,zo+za+zb+zc,
-                  xo+xc,yo+yc,zo+zc)]
-        return s
-    
-class Material:
-    def __init__(self):
-        self._atoms = []
-        self._bonds = []
-        self._uc = None
-
-    def nat(self): return len(self._atoms)
-
-    def atoms(self,i=None):
-        if i is not None: return self._atoms.__getitem__(i)
-        return self._atoms
-    def addatom(self,atom): self._atoms.append(atom)
-    def bonds(self): return self._bonds
-    def uc(self): return self._uc
-    def translate(self,delta):
-        for atom in self._atoms:
-            atom.translate(delta)
-        return
-
-    def center(self):
-        com = self.com()
-        self.translate(-com)
+class Shapes:
+    def __init__(self,molecule,**kwargs):
+        self.atoms = molecule
+        self.find_bonds(**kwargs)
         return
 
     def shapes(self,**kwargs):
         s = []
-        for atom in self.atoms():
-            s.extend(atom.shapes(**kwargs))
-        for bond in self.bonds():
-            s.extend(bond.shapes(**kwargs))
-        if self._uc:
-            s.extend(self._uc.shapes(**kwargs))
+        for atom in self.atoms:
+            s.extend(self.atom_shapes(atom,**kwargs))
+        for bond in self.bonds:
+            s.extend(self.bond_shapes(bond,**kwargs))
         return s
 
-    def find_bonds(self,scalef=0.6):
-        self._bonds = []
-        for i,j in pairs(self.nat()):
-            r = self.atoms(i).dist(self.atoms(j))
-            r0 = self.atoms(i).radius() + self.atoms(j).radius()
-            if r < scalef*r0:
-                self._bonds.append(Bond(self.atoms(i),self.atoms(j)))
-        return
-
-    def com(self):
-        # This is ugly: I should be working with positions, rather
-        # than the underlying ndarrays
-        p = Position()
-        p = p._pos
-        totm = 0
-        for atom in self._atoms:
-            totm += atom.mass()
-            p += atom.mass()*atom.pos()
-        return p/totm
-
-def pairs(n,diag=False):
-    d = 0
-    if diag: d = 1
-    for i in xrange(n):
-        for j in xrange(i+d):
-            yield i,j
-    return
-
-class Atom:
-    def __init__(self,x,y,z,atno):
-        self._type = Element(atno)
-        self._pos = Position(x,y,z)
-        self.ball_stick_scale = 0.3
-        self.ball_scale = 0.9
-
-    def dist(self,other): return self._pos.dist(other._pos)
-    def radius(self): return self._type.radius()
-    def color(self): return self._type.color()
-    def pos(self): return self._pos.get()
-    def mass(self): return self._type.mass()
-
-    def scale(self,style):
-        if style == 'Ball':
-            return self.ball_scale
-        return self.ball_stick_scale
-
-    def translate(self,delta): self._pos.translate(delta)
-
-    def shapes(self,**kwargs):
-        x,y,z = self.pos()
-        r,g,b = self.color()
+    def atom_shapes(self,atom,**kwargs):
+        x,y,z = atom.r
+        r,g,b = atom.color()
         style = kwargs.get('style','BallStick')
-        scaling = self.scale(style)
-        rad = scaling*self.radius()
+        scaling = 0.34
+        rad = scaling*atom.radius()
         return [Sphere(x,y,z,r,g,b,rad)]
 
-class Bond:
-    def __init__(self,start,end):
-        self._start = start
-        self._end = end
-        return
-
-    def shapes(self,**kwargs):
+    def bond_shapes(self,(at1,at2),**kwargs):
         style = kwargs.get('style','BallStick')
         if style == 'Ball': return []
-        x1,y1,z1 = self._start.pos()
-        x2,y2,z2 = self._end.pos()
+        x1,y1,z1 = at1.r
+        x2,y2,z2 = at2.r
         r = kwargs.get('r',0.5)
         g = kwargs.get('g',0.5)
         b = kwargs.get('b',0.5)
         rad = kwargs.get('rad',0.2)
         return [Cylinder(x1,y1,z1,x2,y2,z2,r,g,b,rad)]
+
+    def find_bonds(self,scalef=0.6):
+        from pyquante2.utils import upairs
+        from pyquante2.constants import bohr2ang
+        self.bonds = []
+        for i,j in upairs(xrange(len(self.atoms))):
+            ati,atj = self.atoms[i],self.atoms[j]
+            r = ati.distance(atj)*bohr2ang
+            r0 = ati.radius() + atj.radius()
+            if r < scalef*r0:
+                self.bonds.append((ati,atj))
+        return
+
 
 def test_prims():
     win = TBWindow()
@@ -461,41 +316,15 @@ def test_prims():
     win.run()
     return
 
-def read_xyz(fname):
-    """
-    Read an xyz format file into the sam structure.
-    Currently returns only the final geometry.
-    """
-    f = open(fname)
-    while True:
-        line = f.readline()
-        if not line: break
-        nat = parseline(line,'d')
-        mat = Material()
-        comments = f.readline()
-        for i in range(nat):
-            line = f.readline()
-            sym,x,y,z = parseline(line,'sfff')
-            atno = sym2no[sym]
-            mat.addatom(Atom(x,y,z,atno))
-    mat.find_bonds()
-    mat.center()
-    return mat
-
-def test_read():
-    aspirin = "../Geos/aspirin.xyz"
-    benzene = "../Geos/benzene.xyz"
-    caffeine = "../Geos/caffeine.xyz"
+def test_mol():
+    from pyquante2 import h2o
+    shapes = Shapes(h2o)
     win = TBWindow()
-    mat = read_xyz(aspirin)
-    shapes = mat.shapes()
-    win.calllist(shapes)
+    win.calllist(shapes.shapes())
     win.run()
     return
 
-def main():
-    #test_prims()
-    test_read()
-    return
 
-if __name__=="__main__": main()
+if __name__ == '__main__':
+    #test_prims()
+    test_mol()
