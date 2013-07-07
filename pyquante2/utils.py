745c3d96cf2c43b96975e5b1e8e9874c28c1d71e
Rick Muller <rpmuller@gmail.com>
2013-07-07 08:51:55 -0600

    Colorscale function from Python Cookbook

--- pyquante2/utils.py
+++ pyquante2/utils.py
@@ -199,6 +199,21 @@ def parseline(line,format):
     if len(result) == 1: return result[0]
     return result
 
+def colorscale(mag, cmin, cmax):
+    """
+    Return a tuple of floats between 0 and 1 for R, G, and B.
+    From Python Cookbook (9.11?)
+    """
+    # Normalize to 0-1
+    try:
+        x = float(mag-cmin)/(cmax-cmin)
+    except ZeroDivisionError:
+        x = 0.5  # cmax == cmin
+    blue = min((max((4*(0.75-x), 0.)), 1.))
+    red = min((max((4*(x-0.25), 0.)), 1.))
+    green = min((max((4*abs(x-0.5)-1., 0.)), 1.))
+    return red, green, blue
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
