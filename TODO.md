55472e321b035c126e05cc5d09aa65bf95d4620b
Rick Muller <rpmuller@gmail.com>
2013-07-09 11:14:02 -0600

    More debugging lda errors.

--- TODO.md
+++ TODO.md
@@ -17,9 +17,11 @@ all pgbf or all cgbfs, but there's a bug if I have a mixture.
 **Update**: Fixed for 1e code, not for 2e code.
 
 ## DFT errors
-* The slater basis has a fairly high error (2e-6) in the test suite. I think
-  there may be a bug, perhaps for small densities (guessing).
-* cvwn has a dfb bug that may be similar.
+* The slater basis has a fairly high error (2e-6) in the test suite. 
+  Is there a bug here? The value occurs at high densities. Does not 
+  seem to be an incorrect factor value.
+* cvwn has a similar bug with the dfb value. The erroneous value
+  occurs at 0 values of nb.
 
 ## Next steps to take:
 * Wrap libxc
--- tests/test_dft.py
+++ tests/test_dft.py
@@ -12,17 +12,19 @@ class test_dft(unittest.TestCase):
         max_f = np.max(fa+fb-data['xs'][:,5])
         max_dfa = np.max(dfa-data['xs'][:,6])
         max_dfb = np.max(dfb-data['xs'][:,7])
+        #print np.column_stack([na,nb,data['xs'][:,5]-fa-fb])
         self.assertAlmostEqual(max_f,0,5) ## Fix this!
         self.assertAlmostEqual(max_dfa,0)
         self.assertAlmostEqual(max_dfb,0)
 
     def test_cvwn(self):
-        na = data['xs'][:,0]
-        nb = data['xs'][:,1]
+        na = data['cvwn'][:,0]
+        nb = data['cvwn'][:,1]
         f,dfa,dfb = cvwn(na,nb)
         max_f = np.max(f-data['cvwn'][:,5])
         max_dfa = np.max(dfa-data['cvwn'][:,6])
         max_dfb = np.max(dfb-data['cvwn'][:,7])
+        print np.column_stack([na,nb,data['cvwn'][:,7]-dfb])
         self.assertAlmostEqual(max_f,0)
         self.assertAlmostEqual(max_dfa,0)
         #self.assertAlmostEqual(max_dfb,0,6) ## Fix this!
