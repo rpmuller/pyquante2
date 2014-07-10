ec7a030a3fdec6e5930aaa6e606c289d5ddecd51
Rick Muller <rpmuller@gmail.com>
2014-07-10 09:37:34 -0600

    Moved shell information into normal basis sets

--- pyquante2/basis/basisset.py
+++ pyquante2/basis/basisset.py
@@ -2,20 +2,37 @@
 Basis set constructor
 
 >>> from pyquante2.geo.samples import h
->>> print(basisset(h,'sto3g'))
+>>> bfs = basisset(h,'sto3g')
+>>> print bfs
 cgbf((0.0, 0.0, 0.0),(0, 0, 0),[3.42525091, 0.62391373, 0.1688554],[0.1543289707029839, 0.5353281424384733, 0.44463454202535485])
-
+>>> print bfs.shells
+    [S shell: [3.42525091, 0.62391373, 0.1688554],[0.15432897, 0.53532814, 0.44463454]]
 """
 
 import numpy as np
 from pyquante2 import settings
 from pyquante2.basis import data
 from pyquante2.basis.cgbf import cgbf
-from pyquante2.basis.tools import sym2pow,sym2am
+from pyquante2.basis.tools import sym2pow,sym2am,am2pow,am2sym
+
+class shell(object):
+    def __init__(self,am,origin,exps,coefs):
+        self.am = am
+        self.origin = origin
+        self.exps = exps
+        self.coefs = coefs
+        self.bfs = []
+        for power in am2pow[am]:
+            self.bfs.append(cgbf(origin,power,exps,coefs))
+        return
+
+    def __repr__(self):
+        return "%s shell: %s,%s" % (am2sym[self.am],self.exps,self.coefs)
 
 class basisset(object):
     def __init__(self,atoms,name='sto3g',**kwargs):
         self.bfs = []
+        self.shells = []
         self.name = name
         basis_data = data.basis[name]
         omit_f = kwargs.get('omit_f',settings.omit_f)
@@ -24,6 +41,7 @@ class basisset(object):
                 if omit_f and sym == 'F': continue
                 exps = [e for e,c in prims]
                 coefs = [c for e,c in prims]
+                self.shells.append(shell(sym2am[sym],atom.r,exps,coefs))
                 for power in sym2pow[sym]:
                     self.bfs.append(cgbf(atom.r,power,exps,coefs))
         return
