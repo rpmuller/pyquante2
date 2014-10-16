8f10791d0a713692c44d117f1d0008e493c7814f
Rick Muller <rpmuller@gmail.com>
2014-10-16 08:57:40 -0600

    Added uhf example to scf_simple

--- tests/scf_simple.py
+++ tests/scf_simple.py
@@ -3,13 +3,17 @@ This is the simplest form of the SCF procedure. I find the current code with ite
 and hamiltonians can be counterintuitive and hard to play with.
 """
 import numpy as np
-from pyquante2.geo.samples import h2o,lih
+from pyquante2.geo.samples import h2o,lih,oh,li
 from pyquante2.basis.basisset import basisset
 from pyquante2.utils import trace2,geigh,dmat
 from pyquante2.ints.integrals import onee_integrals, twoe_integrals
 from pyquante2.ints.two import ERI
 
 def scf_simple(geo,basisname='sto3g',maxiter=25,verbose=False):
+    if geo.nopen() == 0: return rhf_simple(geo,basisname,maxiter,verbose)
+    return uhf_simple(geo,basisname,maxiter,verbose)
+
+def rhf_simple(geo,basisname='sto3g',maxiter=25,verbose=False):
     bfs = basisset(geo,basisname)
     i1 = onee_integrals(bfs,geo)
     i2 = twoe_integrals(bfs)
@@ -42,6 +46,46 @@ def scf_simple(geo,basisname='sto3g',maxiter=25,verbose=False):
         print ("Warning: Maxiter %d hit in scf_simple" % maxiter)
     return Energy,E,U
 
+def uhf_simple(geo,basisname='sto3g',maxiter=25,verbose=False):
+    if geo.nopen() == 0: return scf_simple(geo,basisname,maxiter,verbose)
+    bfs = basisset(geo,basisname)
+    i1 = onee_integrals(bfs,geo)
+    i2 = twoe_integrals(bfs)
+    h = i1.T + i1.V
+    E,U = geigh(h,i1.S)
+    Enuke = geo.nuclear_repulsion()
+    Eold = Energy = 0
+    ca = cb = U
+    na,nb = geo.nup(),geo.ndown()
+
+    for i in xrange(maxiter):
+        Energy = Enuke
+        Da = dmat(ca,na)
+        Db = dmat(cb,nb)
+        h = i1.T + i1.V
+        Energy += trace2(Da+Db,h)/2
+        Ja,Ka = i2.get_j(Da),i2.get_k(Da)
+        Jb,Kb = i2.get_j(Db),i2.get_k(Db)
+        Fa = h + Ja + Jb - Ka
+        Fb = h + Ja + Jb - Kb
+        orbea,ca = geigh(Fa,i1.S)
+        orbeb,cb = geigh(Fb,i1.S)
+        Energy += trace2(Fa,Da)/2 + trace2(Fb,Db)/2
+        print ("UHF: %d   %10.4f : %10.4f" % ((i+1),Energy,Enuke))
+        if np.isclose(Energy,Eold):
+            break
+        Eold = Energy
+    else:
+        print ("Warning: Maxiter %d hit in scf_simple" % maxiter)
+    return Energy,E,U
+    
+
 if __name__ == '__main__':
+    print "LiH energy should be -7.8607"
     scf_simple(lih)
+    print "H2O energy should be -74.9598"
     scf_simple(h2o)
+    print "OH energy should be -73.3602"
+    scf_simple(oh)
+    print "Li energy should be -7.3155"
+    scf_simple(li)
