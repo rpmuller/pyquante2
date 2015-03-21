68e0c6e61aa0300bc447fe180719db4f8b5343d9
Rick Muller <rpmuller@gmail.com>
2015-03-20 20:17:16 -0600

    Minor changes

--- pyquante2/dft/dft.py
+++ pyquante2/dft/dft.py
@@ -2,7 +2,7 @@ from pyquante2.dft.functionals import xs
 
 xc_function_map = dict(xs=xs)
 
-def get_xc(grid,D,xcname,nel):
+def get_xc(grid,D,nel,xcname):
     xc_function = xc_function_map(xcname)
     rho = grid.getdens(D)
     
