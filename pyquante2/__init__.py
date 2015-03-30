e7d04a5b5a78f5206350b88047a83b3cb2bacb74
Rick Muller <rmuller@sandia.gov>
2015-03-30 10:35:24 -0600

    Increased the number of pyq2 level imports to make code simpler

--- pyquante2/__init__.py
+++ pyquante2/__init__.py
@@ -1,13 +1,16 @@
 from pyquante2.basis.basisset import basisset
 from pyquante2.basis.cgbf import cgbf,sto
 from pyquante2.basis.pgbf import pgbf
+from pyquante2.dft.dft import get_xc
 from pyquante2.geo.molecule import molecule
 from pyquante2.geo.samples import *
 from pyquante2.graphics.vtkplot import vtk_orbs
 from pyquante2.grid.grid import grid
 from pyquante2.ints.one import S,T,V
+from pyquante2.ints.integrals import onee_integrals,twoe_integrals
 from pyquante2.pt.mp2 import mp2
 from pyquante2.scf.hamiltonians import rhf,uhf
+from pyquante2.utils import trace2,geigh,dmat
 
 try:
     import matplotlib
