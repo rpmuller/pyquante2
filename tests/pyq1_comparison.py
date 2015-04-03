dbd0dbf795cf119ccc42cbd02cbc677250df09b6
Rick Muller <rmuller@sandia.gov>
2015-04-03 17:51:23 -0600

    Added a rohf comparison

--- tests/pyq1_comparison.py
+++ tests/pyq1_comparison.py
@@ -2,6 +2,145 @@
 #  the bugs when implementing pyquante2.
 
 import numpy as np
+
+def pyq1_rohf(atomtuples=[(2,(0,0,0))],basis = '6-31G**',maxit=10,mult=3):
+    from PyQuante import Ints,settings,Molecule
+    from PyQuante.hartree_fock import get_energy
+    from PyQuante.MG2 import MG2 as MolecularGrid
+    from PyQuante.LA2 import mkdens,geigh,trace2,simx
+    from PyQuante.Ints import getJ,getK
+    
+    print ("PyQ1 ROHF run")
+    atoms = Molecule('Pyq1',atomlist=atomtuples,multiplicity=mult)
+
+    bfs = Ints.getbasis(atoms,basis=basis)
+    S,h,I2e = Ints.getints(bfs,atoms)
+
+    nbf = norbs = len(bfs)
+    nel = atoms.get_nel()
+
+    nalpha,nbeta = atoms.get_alphabeta()
+
+    enuke = atoms.get_enuke()
+    orbe,orbs = geigh(h,S)
+    eold = 0
+
+    for i in range(maxit):
+        Da = mkdens(orbs,0,nalpha)
+        Db = mkdens(orbs,0,nbeta)
+        Ja = getJ(I2e,Da)
+        Jb = getJ(I2e,Db)
+        Ka = getK(I2e,Da)
+        Kb = getK(I2e,Db)
+
+        Fa = h+Ja+Jb-Ka
+        Fb = h+Ja+Jb-Kb
+        energya = get_energy(h,Fa,Da)
+        energyb = get_energy(h,Fb,Db)
+        eone = (trace2(Da,h) + trace2(Db,h))/2
+        etwo = (trace2(Da,Fa) + trace2(Db,Fb))/2
+        energy = (energya+energyb)/2 + enuke
+        print i,energy,eone,etwo,enuke
+        if abs(energy-eold) < 1e-5: break
+        eold = energy
+
+        Fa = simx(Fa,orbs)
+        Fb = simx(Fb,orbs)
+        # Building the approximate Fock matrices in the MO basis
+        F = 0.5*(Fa+Fb)
+        K = Fb-Fa
+
+        # The Fock matrix now looks like
+        #      F-K    |  F + K/2  |    F
+        #   ---------------------------------
+        #    F + K/2  |     F     |  F - K/2
+        #   ---------------------------------
+        #       F     |  F - K/2  |  F + K
+
+        # Make explicit slice objects to simplify this
+        do = slice(0,nbeta)
+        so = slice(nbeta,nalpha)
+        uo = slice(nalpha,norbs)
+        F[do,do] -= K[do,do]
+        F[uo,uo] += K[uo,uo]
+        F[do,so] += 0.5*K[do,so]
+        F[so,do] += 0.5*K[so,do]
+        F[so,uo] -= 0.5*K[so,uo]
+        F[uo,so] -= 0.5*K[uo,so]
+
+        orbe,mo_orbs = np.linalg.eigh(F)
+        orbs = np.dot(orbs,mo_orbs)
+    return energy,orbe,orbs
+def pyq1_rohf(atomtuples=[(2,(0,0,0))],basis = '6-31G**',maxit=10,mult=3):
+    from PyQuante import Ints,settings,Molecule
+    from PyQuante.hartree_fock import get_energy
+    from PyQuante.MG2 import MG2 as MolecularGrid
+    from PyQuante.LA2 import mkdens,geigh,trace2,simx
+    from PyQuante.Ints import getJ,getK
+    
+    print ("PyQ1 ROHF run")
+    atoms = Molecule('Pyq1',atomlist=atomtuples,multiplicity=mult)
+
+    bfs = Ints.getbasis(atoms,basis=basis)
+    S,h,I2e = Ints.getints(bfs,atoms)
+
+    nbf = norbs = len(bfs)
+    nel = atoms.get_nel()
+
+    nalpha,nbeta = atoms.get_alphabeta()
+
+    enuke = atoms.get_enuke()
+    orbe,orbs = geigh(h,S)
+    eold = 0
+
+    for i in range(maxit):
+        Da = mkdens(orbs,0,nalpha)
+        Db = mkdens(orbs,0,nbeta)
+        Ja = getJ(I2e,Da)
+        Jb = getJ(I2e,Db)
+        Ka = getK(I2e,Da)
+        Kb = getK(I2e,Db)
+
+        Fa = h+Ja+Jb-Ka
+        Fb = h+Ja+Jb-Kb
+        energya = get_energy(h,Fa,Da)
+        energyb = get_energy(h,Fb,Db)
+        eone = (trace2(Da,h) + trace2(Db,h))/2
+        etwo = (trace2(Da,Fa) + trace2(Db,Fb))/2
+        energy = (energya+energyb)/2 + enuke
+        print i,energy,eone,etwo,enuke
+        if abs(energy-eold) < 1e-5: break
+        eold = energy
+
+        Fa = simx(Fa,orbs)
+        Fb = simx(Fb,orbs)
+        # Building the approximate Fock matrices in the MO basis
+        F = 0.5*(Fa+Fb)
+        K = Fb-Fa
+
+        # The Fock matrix now looks like
+        #      F-K    |  F + K/2  |    F
+        #   ---------------------------------
+        #    F + K/2  |     F     |  F - K/2
+        #   ---------------------------------
+        #       F     |  F - K/2  |  F + K
+
+        # Make explicit slice objects to simplify this
+        do = slice(0,nbeta)
+        so = slice(nbeta,nalpha)
+        uo = slice(nalpha,norbs)
+        F[do,do] -= K[do,do]
+        F[uo,uo] += K[uo,uo]
+        F[do,so] += 0.5*K[do,so]
+        F[so,do] += 0.5*K[so,do]
+        F[so,uo] -= 0.5*K[so,uo]
+        F[uo,so] -= 0.5*K[uo,so]
+
+        orbe,mo_orbs = np.linalg.eigh(F)
+        orbs = np.dot(orbs,mo_orbs)
+    return energy,orbe,orbs
+    
+
 def pyq1_dft(atomtuples=[(2,(0,0,0))],basis = '6-31G**',maxit=10,
              xcname='SVWN'):
     from PyQuante import Ints,settings,Molecule
@@ -16,7 +155,6 @@ def pyq1_dft(atomtuples=[(2,(0,0,0))],basis = '6-31G**',maxit=10,
     bfs = Ints.getbasis(atoms,basis=basis)
     S,h,Ints = Ints.getints(bfs,atoms)
 
-    nel = atoms.get_nel()
     nclosed,nopen = nel//2,nel%2
     assert nopen==0
     enuke = atoms.get_enuke()
@@ -65,6 +203,54 @@ def func_compare():
     plt.plot(ns,dfc1,label='df_vwn5/pyq1')
     plt.show()
 
+def pyq2_rohf(atomtuples=[(2,0,0,0)],basis = '6-31G**',maxit=10,xcname='svwn',
+              mult=3):
+    import pyquante2 as pyq2
+    print ("pyq2 ROHF run")
+    geo = pyq2.molecule(atomtuples,multiplicity=mult)
+    bfs = pyq2.basisset(geo,name=basis)
+    i1 = pyq2.onee_integrals(bfs,geo)
+    i2 = pyq2.twoe_integrals(bfs)
+    h = i1.T + i1.V
+    orbe,orbs = pyq2.geigh(h,i1.S)
+    eold = 0
+    E0 = geo.nuclear_repulsion()
+    nalpha,nbeta = geo.nup(),geo.ndown()
+    norbs = len(bfs)
+
+    for i in range(maxit):
+        Da = pyq2.dmat(orbs,nalpha)
+        Db = pyq2.dmat(orbs,nbeta)
+        E1 = 0.5*pyq2.trace2(Da+Db,h)
+        Ja,Ka = i2.get_j(Da),i2.get_k(Da)
+        Jb,Kb = i2.get_j(Db),i2.get_k(Db)
+        Fa = h + Ja + Jb - Ka
+        Fb = h + Ja + Jb - Kb
+        E2 = 0.5*(pyq2.trace2(Fa,Da)+pyq2.trace2(Fb,Db))
+        energy = E0+E1+E2
+        print (energy,E1,E2,E0)
+
+        Fa = pyq2.utils.simx(Fa,orbs)
+        Fb = pyq2.utils.simx(Fb,orbs)
+
+        F = 0.5*(Fa+Fb)
+        K = Fb-Fa
+        # Make explicit slice objects to simplify this
+        do = slice(0,nbeta)
+        so = slice(nbeta,nalpha)
+        uo = slice(nalpha,norbs)
+        F[do,do] -= K[do,do]
+        F[uo,uo] += K[uo,uo]
+        F[do,so] += 0.5*K[do,so]
+        F[so,do] += 0.5*K[so,do]
+        F[so,uo] -= 0.5*K[so,uo]
+        F[uo,so] -= 0.5*K[uo,so]
+
+        E,cmo = np.linalg.eigh(F)
+        orbs = np.dot(orbs,cmo)
+
+    return
+
 def pyq2_dft(atomtuples=[(2,0,0,0)],basis = '6-31G**',maxit=10,xcname='svwn'):
     import pyquante2 as pyq2
     print ("pyq2 DFT run")
@@ -98,11 +284,12 @@ def pyq2_dft(atomtuples=[(2,0,0,0)],basis = '6-31G**',maxit=10,xcname='svwn'):
             break
         eold = energy
     return energy
-        
     
 
 if __name__ == '__main__':
-    pyq1_dft()
-    pyq2_dft()
+    pyq1_rohf()
+    pyq2_rohf()
+    #pyq1_dft()
+    #pyq2_dft()
     #func_compare()
     
