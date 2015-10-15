850b8ba9bc176b8769edce7e94db1dbde9585691
Sam Manzer <samuel.manzer1990@gmail.com>
2015-10-15 16:44:16 -0700

    Fixed major errors - now passes test and correctly integrates MOs for big systems, but is slower

--- cython/cbecke.pyx
+++ cython/cbecke.pyx
@@ -1,16 +1,12 @@
 # cython: boundscheck=False, wraparound=False, cdivision=True
 import numpy as np
 cimport numpy as np
-from cpython cimport array
-import array
 from pyquante2.grid.data import Bragg
-
-np.import_array()
 
 def becke_reweight_atoms(atoms,agrids,**kwargs):
     cdef int do_becke_hetero = kwargs.get('do_becke_hetero',True)
 
-    cdef double[:] cBragg = array.array('d',(0 for _ in xrange(len(Bragg))))
+    cdef double[:] cBragg = np.empty((len(Bragg)),'d')
     cBragg[0] = 0
     for idx in xrange(1,len(Bragg)):
         cBragg[idx] = Bragg[idx]
@@ -21,28 +17,32 @@ def becke_reweight_atoms(atoms,agrids,**kwargs):
         for j in xrange(len(atoms)):
             rijs[i,j] = np.linalg.norm(atoms[i].r-atoms[j].r)
 
-    max_agrid_npts = max(agrid.npts for agrid in agrids)
-    cdef double[:,:] rjps = np.ndarray((len(atoms),max_agrid_npts),'d',order="C")
-    for at,agrid in enumerate(agrids):
-        for p in xrange(agrid.npts):
-            xyzp = agrid.points[p,:3]
-            rjps[at,p] = np.linalg.norm(atoms[at].r-xyzp)
+    npts = sum(agrid.npts for agrid in agrids)
+    cdef long g
+    cdef long g_abs = 0
+    cdef double[:,:] rjps = np.ndarray((len(atoms),npts),'d',order="C")
+    for at in xrange(len(atoms)):
+        g_abs = 0
+        for agrid in agrids:
+            for g in xrange(agrid.npts):
+                xyzp = agrid.points[g,:3]
+                rjps[at,g_abs] = np.linalg.norm(atoms[at].r-xyzp)
+                g_abs += 1
 
     cdef int[:] atnos = np.empty(len(atoms),dtype=np.intc) 
     for iat in xrange(0,len(atoms)):
         atnos[iat] = atoms[iat].atno
 
     Ps = np.empty(len(atoms),'d')
-    cdef long g
     cdef long jat
-
+    g_abs = 0
     for iat,agrid in enumerate(agrids):
         for g from 0 <= g < agrid.npts:
-            xyzp = agrid.points[i,:3]
             for jat from 0 <= jat < len(atoms):
-                Ps[jat] = becke_atomic_grid_p(jat,g,rijs,rjps,atnos,do_becke_hetero,cBragg)
+                Ps[jat] = becke_atomic_grid_p(jat,g_abs,rijs,rjps,atnos,do_becke_hetero,cBragg)
             P = Ps[iat]/sum(Ps)
             agrid.points[g,3] = P*agrid.points[g,3]
+            g_abs += 1
     return
 
 cdef inline double becke_atomic_grid_p(long jat,long g,double[:,:] rijs,double[:,:] rjps,int[:] atnos,int do_becke_hetero,double[:] cBragg):
