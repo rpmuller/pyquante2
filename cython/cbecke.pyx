289ec441918077a9f5f8986cb568940813c23e19
Sam Manzer <samuel.manzer1990@gmail.com>
2015-10-15 18:17:58 -0700

    fast again, only about ten percent slower than peak

--- cython/cbecke.pyx
+++ cython/cbecke.pyx
@@ -2,6 +2,8 @@
 import numpy as np
 cimport numpy as np
 from pyquante2.grid.data import Bragg
+import time
+from libc.math cimport sqrt
 
 def becke_reweight_atoms(atoms,agrids,**kwargs):
     cdef int do_becke_hetero = kwargs.get('do_becke_hetero',True)
@@ -11,34 +13,65 @@ def becke_reweight_atoms(atoms,agrids,**kwargs):
     for idx in xrange(1,len(Bragg)):
         cBragg[idx] = Bragg[idx]
 
+    cdef long nat = len(atoms)
+    cdef double[:,:] rs = np.empty((nat,3))
+    for i in xrange(nat):
+        for j in xrange(3):
+            rs[i,j] = atoms[i].r[j]
+
     # Precompute norms outside of inner loop
-    cdef double[:,:] rijs = np.ndarray((len(atoms),len(atoms)),'d',order="C")
-    for i in xrange(len(atoms)):
-        for j in xrange(len(atoms)):
-            rijs[i,j] = np.linalg.norm(atoms[i].r-atoms[j].r)
+    cdef double[:,:] rijs = np.empty((nat,nat),'d')
+    cdef double diff 
+    for i in xrange(nat):
+        for j in xrange(nat):
+            rijs[i,j] = 0.0
+            diff = (rs[i][0]-rs[j][0])
+            rijs[i,j] += diff*diff
+            diff = (rs[i][1]-rs[j][1])
+            rijs[i,j] += diff*diff
+            diff = (rs[i][2]-rs[j][2])
+            rijs[i,j] += diff*diff
+            rijs[i,j] = sqrt(rijs[i,j])
 
     npts = sum(agrid.npts for agrid in agrids)
+
+    t0 = time.time()
+    # Need list of all grid points of all atoms
     cdef long g
     cdef long g_abs = 0
-    cdef double[:,:] rjps = np.ndarray((len(atoms),npts),'d',order="C")
-    for at in xrange(len(atoms)):
-        g_abs = 0
-        for agrid in agrids:
-            for g in xrange(agrid.npts):
-                xyzp = agrid.points[g,:3]
-                rjps[at,g_abs] = np.linalg.norm(atoms[at].r-xyzp)
-                g_abs += 1
+    cdef double[:,:] all_points = np.empty((npts,3))
+    for agrid in agrids:
+        for g in xrange(agrid.npts):
+            all_points[g_abs,0] = agrid.points[g,0]
+            all_points[g_abs,1] = agrid.points[g,1]
+            all_points[g_abs,2] = agrid.points[g,2]
+            g_abs += 1
+    t1 = time.time()
+    print "0:"
+    print t1 - t0
 
-    cdef int[:] atnos = np.empty(len(atoms),dtype=np.intc) 
-    for iat in xrange(0,len(atoms)):
+    cdef double[:,:] rjps = np.empty((nat,npts),'d',order="C")
+    for at in xrange(nat):
+        for g in xrange(npts):
+            rjps[at,g] = 0.0
+            for k in xrange(3):
+                diff = rs[at,k] - all_points[g,k]
+                rjps[at,g] += diff*diff
+            rjps[at,g] = sqrt(rjps[at,g])
+    t2 = time.time()            
+    print "1: "
+    print t2 - t1
+
+    cdef int[:] atnos = np.empty(nat,dtype=np.intc) 
+    for iat in xrange(nat):
         atnos[iat] = atoms[iat].atno
 
-    Ps = np.empty(len(atoms),'d')
+    Ps = np.empty(nat,'d')
     cdef long jat
     g_abs = 0
     for iat,agrid in enumerate(agrids):
         for g from 0 <= g < agrid.npts:
-            for jat from 0 <= jat < len(atoms):
+            for jat from 0 <= jat < nat:
                 Ps[jat] = becke_atomic_grid_p(jat,g_abs,rijs,rjps,atnos,do_becke_hetero,cBragg)
             P = Ps[iat]/sum(Ps)
             agrid.points[g,3] = P*agrid.points[g,3]
