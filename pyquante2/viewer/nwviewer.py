3df3495b4f515be8913f4493b277bf82d6f72d19
Rick Muller <rpmuller@gmail.com>
2017-04-08 09:49:42 -0400

    Started a new viewer specific to nwchem

---
+++ pyquante2/viewer/nwviewer.py
@@ -0,0 +1,16 @@
+from pyquante2.viewer import Viewer
+
+class NWViewer(Viewer):
+    def __init__(self):
+        Viewer.__init__(self)
+        return
+
+def run():
+    win = NWViewer()
+    # Consider parsing args here
+    win.run()
+    return
+
+if __name__ == '__main__': run()
+
+
