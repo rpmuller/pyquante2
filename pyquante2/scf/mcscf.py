7f125a81c71e138ca39fa47c8360b5df50dc27d0
Rick Muller <rpmuller@gmail.com>
2017-05-20 17:08:38 -0400

    More comments for mcscf module

--- pyquante2/scf/mcscf.py
+++ pyquante2/scf/mcscf.py
@@ -20,4 +20,36 @@ Here
   i,j   indices of orbitals
   m,n   indices of basis functions
 
+With different choices for the f,a,b terms, this energy expression can
+describe
+  - Closed shell RHF
+  - Open shell ROHF
+  - Generalized valence bond MC-SCF
+  - More general MC-SCFs, CAS-SCF, etc.
+We will start with the first three of these, and generalize to the fourth
+when all is working with those.
+
+A good reference to the general method is:
+The Self-Consistent Equations for Generalized Valence Bond and Open-Shell
+Hartree-Fock Wave Functions
+Frank W. Bobrowicz and William A. Goddard, III, in
+Methods of Electronic Structure Theory, H.F. Schaeffer, III, ed.
+Plenum Publishing Corp., 1977
+http://www.wag.caltech.edu/publications/sup/pdf/108.pdf
+
+For RHF, ROHF, and GVB wave functions, the f,a,b terms take a fairly
+simple form:
+
+  f_i = 1      If the orbital is closed-shell (doubly occupied)
+  f_i = 1/2    If the orbital is open-shell (singly-occupied, high spin)
+  f_i = c_i^2  If the orbital is part of a GVB pair with coefficient c_i
+
+  a_ij = 2 f_i f_j
+  b_ij = -f_i f_j
+
+except that:
+  b_ij = -1/2               If i and j are both open-shell
+  a_ii = f_i, b_ii = 0      If i is a pair orbital
+  a_ij = 0, b_ij = -c_i c_j If i and j are in the same pair
+
 """
