01e8d8cfc016975b1df5fe7ce2ab70010ce3dd48
Rick Muller <rpmuller@gmail.com>
2017-05-21 16:07:29 -0400

    Added OCBSE routines

--- pyquante2/scf/mcscf.py
+++ pyquante2/scf/mcscf.py
@@ -80,8 +80,82 @@ def rhf(geo,basisname='sto3g',maxiter=25,verbose=False):
     assert nopen == 0
     npair = 0
     ncore = geo.nclosed() - npair
+    nocc = ncore + nopen + 2*npair
+    norb = len(bfs)
+    virt = range(nocc,norb)
+    orbs_per_shell = get_orbs_per_shell(ncore,nopen,npair)
+    nsh = len(orbs_per_shell)
+    shells = orbital_to_shell_mapping(ncore,nopen,npair)
+    
+    #TODO: Make fab range over shells, not orbs
+    #TODO: make a shell function that maps orbitals to shells
+    f,a,b = fab(ncore,nopen,npair)
 
+    Eold = 0
+    for it in range(maxiter):
+        # Make all of the density matrices:
+        Ds = [dmat_gen(U,orbs) for orbs in orbs_per_shell]
+        # Make the 2e matrices
+        Js = [i2.get_j(D) for D in Dmats]
+        Ks = [i2.get_j(K) for D in Dmats]
+        # Make the Fock matrices
+        Fs = [f[i]*h + sum(a[i,j]*Js[j] + b[i,j]*Ks[j] for j in range(nsh))
+              for i in range(nsh)]
+
+        # Compute the one-electron part of the energy
+        hmo = ao2mo(h,U)
+        Eone = sum(f[shell[i]]*hmo[i,i] for i in range(nocc))
+
+        # Perform the OCBSE step
+        E = Eone
+        Unew = np.zeros(U.shape,'d') # We'll put the new orbitals here
+        for i,orbs in enumerate(orbs_per_shell):
+            space = orbs + virts
+            F = ao2mo(H,U[:,space])
+            Ei,C = eigh(F)
+            E += sum(Ei[orbs])
+            Ui = np.dot(C.T,U[:,space])
+            Unew[:,space] = Ui
+        U = Unew
+
+        # Perform the ROTION step:
+
+        
+        print "Iteration %d: Energy %.2f" % (it,E)
+        if np.isclose(E,Eold):
+            print "Energy converged"
+            break
+    else:
+        print "Maximum iterations (%d) reached without convergence" % (maxit)
     return
+
+def orbital_to_shell_mapping(ncore,nopen,npair):
+    """\
+    Map the orbitals to shells. All the core orbitals are in
+    the first shell. Then each orbital has its own shell.
+    >>> orbital_to_shell_mapping(1,0,0)
+    [0]
+    >>> orbital_to_shell_mapping(2,0,0)
+    [0, 0]
+    >>> orbital_to_shell_mapping(2,1,0)
+    [0, 0, 1]
+    """
+    shell = [0 for i in range(ncore)]
+    ncoreshells = 1 if ncore else 0
+    for i in range(nopen+2*npair):
+        shell.append(i+ncoreshells)
+    return shell
+
+def dmat_gen(c,indices): return np.dot(c[:,indices],c[:,indices].T)
+
+def get_orbs_per_shell(ncore,nopen,npair):
+    nocc = ncore+nopen+2*npair
+    orbs_per_shell = []
+    if ncore:
+        orbs_per_shell.append(range(ncore))
+    for i in range(ncore,nocc):
+        orbs_per_shell.append([i])
+    return orbs_per_shell
 
 def guess_gvb_ci_coeffs(npair):
     """
@@ -130,10 +204,10 @@ def fab(ncore,nopen,npair,coeffs=None):
     array([[ 0., -0.],
            [-0.,  0.]])
     """
-    norb = ncore+nopen+2*npair
-    f = np.zeros(norb,'d')
-    a = np.zeros((norb,norb),'d')
-    b = np.zeros((norb,norb),'d')
+    nocc = ncore+nopen+2*npair
+    f = np.zeros(nocc,'d')
+    a = np.zeros((nocc,nocc),'d')
+    b = np.zeros((nocc,nocc),'d')
 
     if npair > 0 and coeffs is None:
         coeffs = guess_gvb_ci_coeffs(npair)
@@ -154,8 +228,8 @@ def fab(ncore,nopen,npair,coeffs=None):
         f[i+npair] = coeffs[2*p+1]
 
     # Basic a,b assumptions:
-    for i in range(norb):
-        for j in range(norb):
+    for i in range(nocc):
+        for j in range(nocc):
             a[i,j] = 2*f[i]*f[j]
             b[i,j] = -f[i]*f[j]
             
