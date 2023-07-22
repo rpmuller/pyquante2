95f88fa6cea085b68af03dece386c60083f48324
Rick Muller <rpmuller@gmail.com>
2023-07-22 12:23:00 -0600

    Minor changes

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,6 +1,9 @@
 import unittest
 from pyquante2.basis.pgbf import pgbf
 from pyquante2.ints.one import S,T,V
+from pyquante2.cints.one import S as cS
+from pyquante2.cints.one import T as cT
+from pyquante2.cints.one import V as cV
 
 class test_one(unittest.TestCase):
     def test_pgbf(self):
@@ -10,16 +13,20 @@ class test_one(unittest.TestCase):
     def test_overlap(self):
         s = pgbf(1.0)
         self.assertAlmostEqual(S(s,s),1.0)
+        self.assertAlmostEqual(cS(s,s),1.0)
 
     def test_kinetic(self):
         s = pgbf(1.0)
         self.assertAlmostEqual(T(s,s),1.5)
+        self.assertAlmostEqual(cT(s,s),1.5)
 
     def test_nuclear(self):
         s = pgbf(1.0)
         self.assertAlmostEqual(V(s,s,((0,0,0))),-1.5957691216057328)
+        self.assertAlmostEqual(cV(s,s,((0,0,0))),-1.5957691216057328)
 
     def test_h_atom(self):
         "Single primitive approximation to H atom"
         s = pgbf(0.285)
         self.assertAlmostEqual(T(s,s)+V(s,s,((0,0,0))),-0.424407589178)
+        self.assertAlmostEqual(cT(s,s)+cV(s,s,((0,0,0))),-0.424407589178)
---
+++ tests/test_two.py
@@ -0,0 +1,86 @@
+import unittest
+from pyquante2.basis.pgbf import pgbf
+from pyquante2.ints.two import ERI as coul_huz
+from pyquante2.ints.hgp import ERI as coul_hgp
+from pyquante2.cints.two import ERI as coul_chuz
+from pyquante2.cints.hgp import ERI as coul_chgp
+
+s = pgbf(1.0)
+px = pgbf(1.0,(0,0,0),(1,0,0))
+
+class test_two(unittest.TestCase):
+    def test_huz(self):
+        self.assertAlmostEqual(coul_huz(s,s,px,px),0.9403159725793302)
+        self.assertEqual(coul_huz(s,s,s,px),0)
+
+    def test_hgp(self):
+        self.assertAlmostEqual(coul_hgp(s,s,px,px),0.9403159725793302)
+        self.assertEqual(coul_hgp(s,s,s,px),0)
+
+    def test_chuz(self):
+        self.assertAlmostEqual(coul_chuz(s,s,px,px),0.9403159725793302)
+        self.assertEqual(coul_chuz(s,s,s,px),0)
+
+    def test_chgp(self):
+        self.assertAlmostEqual(coul_chgp(s,s,px,px),0.9403159725793302)
+        self.assertEqual(coul_chgp(s,s,s,px),0)
+
+    def test_huz_range(self):
+        for r,val in [(0.0, 1.1283791670951362),
+                      (1.0, 0.8427007900292186),
+                      (2.0, 0.49766113257563993),
+                      (3.0, 0.33332596983445223),
+                      (4.0, 0.2499999961456855), # coulomb's law hereafter:
+                      (5.0, 1/5), 
+                      (6.0, 1/6),
+                      (7.0, 1/7),
+                      (8.0, 1/8),
+                      (9.0, 1/9)]:
+            s3 = pgbf(1.0, (0.0,0.0,r))
+            self.assertAlmostEqual(coul_huz(s,s,s3,s3),val)
+        return
+
+    def test_hgp_range(self):
+        for r,val in [(0.0, 1.1283791670951362),
+                      (1.0, 0.8427007900292186),
+                      (2.0, 0.49766113257563993),
+                      (3.0, 0.33332596983445223),
+                      (4.0, 0.2499999961456855), # coulomb's law hereafter:
+                      (5.0, 1/5), 
+                      (6.0, 1/6),
+                      (7.0, 1/7),
+                      (8.0, 1/8),
+                      (9.0, 1/9)]:
+            s3 = pgbf(1.0, (0.0,0.0,r))
+            self.assertAlmostEqual(coul_hgp(s,s,s3,s3),val)
+        return
+
+    def test_chuz_range(self):
+        for r,val in [(0.0, 1.1283791670951362),
+                      (1.0, 0.8427007900292186),
+                      (2.0, 0.49766113257563993),
+                      (3.0, 0.33332596983445223),
+                      (4.0, 0.2499999961456855), # coulomb's law hereafter:
+                      (5.0, 1/5), 
+                      (6.0, 1/6),
+                      (7.0, 1/7),
+                      (8.0, 1/8),
+                      (9.0, 1/9)]:
+            s3 = pgbf(1.0, (0.0,0.0,r))
+            self.assertAlmostEqual(coul_chuz(s,s,s3,s3),val)
+        return
+
+    def test_chgp_range(self):
+        for r,val in [(0.0, 1.1283791670951362),
+                      (1.0, 0.8427007900292186),
+                      (2.0, 0.49766113257563993),
+                      (3.0, 0.33332596983445223),
+                      (4.0, 0.2499999961456855), # coulomb's law hereafter:
+                      (5.0, 1/5), 
+                      (6.0, 1/6),
+                      (7.0, 1/7),
+                      (8.0, 1/8),
+                      (9.0, 1/9)]:
+            s3 = pgbf(1.0, (0.0,0.0,r))
+            self.assertAlmostEqual(coul_chgp(s,s,s3,s3),val)
+        return
