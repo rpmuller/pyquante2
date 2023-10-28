5f3fd43c31fac14a026665b75dbba72722239980
Eric Berquist <eric.berquist@gmail.com>
2023-10-27 22:17:22 -0400

    Blacken setup.py

--- setup.py
+++ setup.py
@@ -17,38 +17,47 @@ ext_modules = []
 
 if use_cython:
     ext_modules += [
-        Extension("pyquante2.cints.one",["cython/cone_wrap.pyx","cython/cints.c"]),
-        Extension("pyquante2.cints.two",["cython/ctwo_wrap.pyx","cython/cints.c"]),
-        Extension("pyquante2.cints.hgp",["cython/chgp_wrap.pyx","cython/chgp.c","cython/cints.c"]),
-        Extension("pyquante2.cints.rys",["cython/crys_wrap.pyx","cython/crys.c"]),
-        Extension("pyquante2.cbecke",["cython/cbecke.pyx"],
-                   include_dirs=[np.get_include()])
-        ]
-    cmdclass.update({'build_ext': build_ext})
+        Extension("pyquante2.cints.one", ["cython/cone_wrap.pyx", "cython/cints.c"]),
+        Extension("pyquante2.cints.two", ["cython/ctwo_wrap.pyx", "cython/cints.c"]),
+        Extension(
+            "pyquante2.cints.hgp",
+            ["cython/chgp_wrap.pyx", "cython/chgp.c", "cython/cints.c"],
+        ),
+        Extension("pyquante2.cints.rys", ["cython/crys_wrap.pyx", "cython/crys.c"]),
+        Extension(
+            "pyquante2.cbecke", ["cython/cbecke.pyx"], include_dirs=[np.get_include()]
+        ),
+    ]
+    cmdclass.update({"build_ext": build_ext})
 else:
     ext_modules += [
-        Extension("pyquante2.cints.one",["cython/cone_wrap.c","cython/cints.c"]),
-        Extension("pyquante2.cints.two",["cython/ctwo_wrap.c","cython/cints.c"]),
-        Extension("pyquante2.cints.hgp",["cython/chgp_wrap.c","cython/chgp.c","cython/cints.c"]),
-        Extension("pyquante2.cints.rys",["cython/crys_wrap.c","cython/crys.c"]),
-        ]
+        Extension("pyquante2.cints.one", ["cython/cone_wrap.c", "cython/cints.c"]),
+        Extension("pyquante2.cints.two", ["cython/ctwo_wrap.c", "cython/cints.c"]),
+        Extension(
+            "pyquante2.cints.hgp",
+            ["cython/chgp_wrap.c", "cython/chgp.c", "cython/cints.c"],
+        ),
+        Extension("pyquante2.cints.rys", ["cython/crys_wrap.c", "cython/crys.c"]),
+    ]
 
-with open('README.md') as file:
+with open("README.md") as file:
     long_description = file.read()
 
-setup(name='pyquante2',
-      install_requires=['numpy'],
-      packages=['pyquante2',
-                'pyquante2.basis',
-                'pyquante2.dft',
-                'pyquante2.geo',
-                'pyquante2.graphics',
-                'pyquante2.grid',
-                'pyquante2.ints',
-                'pyquante2.pt',
-                'pyquante2.scf',
-                'pyquante2.viewer',
-                ],
-      cmdclass = cmdclass,
-      ext_modules = ext_modules,
-      )
+setup(
+    name="pyquante2",
+    install_requires=["numpy"],
+    packages=[
+        "pyquante2",
+        "pyquante2.basis",
+        "pyquante2.dft",
+        "pyquante2.geo",
+        "pyquante2.graphics",
+        "pyquante2.grid",
+        "pyquante2.ints",
+        "pyquante2.pt",
+        "pyquante2.scf",
+        "pyquante2.viewer",
+    ],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+)
