a5b4bcc4b010fca63a4500df7a90eabccaacf14e
Eric Berquist <eric.berquist@gmail.com>
2023-11-02 21:59:59 -0400

    Silence NumPy deprecated API warnings during compilation

--- setup.py
+++ setup.py
@@ -6,32 +6,49 @@ from setuptools.extension import Extension
 try:
     from Cython.Build import cythonize
 except ImportError:
-    file_ext = "c"
-    use_cython = False
+    FILE_EXT = "c"
+    USE_CYTHON = False
 else:
     import numpy as np
 
-    file_ext = "pyx"
-    use_cython = True
+    FILE_EXT = "pyx"
+    USE_CYTHON = True
 
 
+_NUMPY_MACROS = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
 ext_modules = [
-    Extension("pyquante2.cints.one", [f"cython/cone_wrap.{file_ext}", "cython/cints.c"]),
-    Extension("pyquante2.cints.two", [f"cython/ctwo_wrap.{file_ext}", "cython/cints.c"]),
+    Extension(
+        "pyquante2.cints.one",
+        [f"cython/cone_wrap.{FILE_EXT}", "cython/cints.c"],
+        define_macros=_NUMPY_MACROS,
+    ),
+    Extension(
+        "pyquante2.cints.two",
+        [f"cython/ctwo_wrap.{FILE_EXT}", "cython/cints.c"],
+        define_macros=_NUMPY_MACROS,
+    ),
     Extension(
         "pyquante2.cints.hgp",
-        [f"cython/chgp_wrap.{file_ext}", "cython/chgp.c", "cython/cints.c"],
+        [f"cython/chgp_wrap.{FILE_EXT}", "cython/chgp.c", "cython/cints.c"],
+        define_macros=_NUMPY_MACROS,
     ),
-    Extension("pyquante2.cints.rys", [f"cython/crys_wrap.{file_ext}", "cython/crys.c"]),
+    Extension(
+        "pyquante2.cints.rys",
+        [f"cython/crys_wrap.{FILE_EXT}", "cython/crys.c"],
+        define_macros=_NUMPY_MACROS,
+    ),
 ]
 
-if use_cython:
+if USE_CYTHON:
     ext_modules.append(
         Extension(
-            "pyquante2.cbecke", ["cython/cbecke.pyx"], include_dirs=[np.get_include()]
+            "pyquante2.cbecke",
+            ["cython/cbecke.pyx"],
+            define_macros=_NUMPY_MACROS,
+            include_dirs=[np.get_include()],
         )
     )
-    ext_modules = cythonize(ext_modules)
+    ext_modules = cythonize(ext_modules, annotate=True)
 
 setup(
     name="pyquante2",
